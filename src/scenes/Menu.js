ba3e5272d7808d02767e89a1db9e16ef30c9429f
RyanP <rpalmber@ucsc.edu>
2021-06-28 22:54:09 -0700

    ActuallyASsets&BGM

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,6 +8,7 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_explosion', './assets/assets_explosion38.wav');
         this.load.audio('sfx_rocket', './assets/assets_rocket_shot.wav');
         this.load.audio('sfx_boomerang', './assets/boomerangthrow.wav');
+        this.load.audio('sfx_BGM', './assets/LonelyWinter.mp3');
     }
     create() {
         let menuConfig = {
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,24 +4,23 @@ class Play extends Phaser.Scene {
     }  
     preload() {
         // load images/tile sprites
-        this.load.image('rocket', './assets/rocket.png');
-        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('rocket', './assets/rocket01.png');
+        // this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
         this.load.image('parallax', './assets/parallax.png');
-        this.load.image('miniship', './assets/MiniRocket.png');
+        this.load.image('miniship', './assets/MiniSpaceship.png');
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        this.load.image('spaceship', './assets/battleship01.png');
+
+
       }
     create() {
         this.starfield=this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         this.parallax=this.add.tileSprite(0, 0, 640, 480, 'parallax').setOrigin(0, 0);
 
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+
         // this.p1Boom = new Boomerang(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
-        this.p1Rocket = new Boomerang(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
 
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
@@ -32,11 +31,38 @@ class Play extends Phaser.Scene {
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
         this.ship04 = new Miniship(this, game.config.width, borderUISize*3 + borderPadding*5, 'miniship', 0, 40).setOrigin(0,0);
- 
+
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        // this.shipAnimation(this.ship01);
         this.anims.create({
             key: 'explode',
             frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
             frameRate: 30
+        // })
+        // this.anims.create({
+        //     key: 'driller1',
+        //     frames: this.anims.generateFrameNumbers('spaceshipanim', { start: 0, end: 0, first: 0}),
+        //     frameRate: 14,
+        //     repeat:-1
+        // })
+
+        // this.anims.create({
+        //     key: 'driller2',
+        //     frames: this.anims.generateFrameNumbers('spaceshipanim', { start: 1, end: 1, first: 1}),
+        //     frameRate: 14,
+        //     repeat:-1
+        // })
+
+        // this.anims.create({
+        //     key: 'driller3',
+        //     frames: this.anims.generateFrameNumbers('spaceshipanim', { start: 2, end: 2, first: 2}),
+        //     frameRate: 14,
+        //     repeat:-1
+        
         });
 
 
@@ -58,7 +84,7 @@ class Play extends Phaser.Scene {
           }
         
             this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
-        
+            this.fire = this.add.text(borderUISize + borderPadding*3, borderUISize + borderPadding*2, 'FIRE', scoreConfig)
 
             
         // GAME OVER flag
@@ -70,7 +96,13 @@ class Play extends Phaser.Scene {
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or ← for Menu', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
-
+        if (!this.gameOver) {
+            this.sound.play('sfx_BGM');
+        }
+        // if (this.gameOver == true) {
+        //     this.sound.pause('sfx_BGM');
+        //     this.sound.currentTime = 0;
+        // }
 
         // Display Time
         // let timerDisplay = {
@@ -92,6 +124,7 @@ class Play extends Phaser.Scene {
             
     }
     update() {
+        
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
             this.scene.restart();
         }
@@ -150,6 +183,35 @@ class Play extends Phaser.Scene {
             return false;
         }
     }
+    // let (p = 0)
+
+    // Here I attempted to animate the spaceships fram by frame, but I couldn't get the syntax right.
+
+    // shipAnimation(ship) {
+    //     let drilly = this.add.sprite(ship.x, ship.y, 'driller').setOrigin(0, 0);
+    //     if (p = 0) {
+
+    //         drilly.anims.play('driller1');
+    //         drilly.on('animationcomplete', () => {
+    //             drilly.destroy();
+    //             p += 1;
+    //         }
+    //     }
+    //     if (p = 1) {
+    //         drilly.anims.play('driller2');
+    //         drilly.on('animationcomplete', () => {
+    //             drilly.destroy();
+    //             p += 1;
+    //         })
+    //     }
+    //     if (p = 2) {
+    //         drilly.anims.play('driller3');
+    //         drilly.on('animationcomplete', () => {
+    //             drilly.destroy();
+    //             p = 0;
+    //     }
+        
+    // }
 
     shipExplode(ship) {
         ship.alpha=0;
